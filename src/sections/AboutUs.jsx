8b71a708952a129893ef81de9478644215ab342d
SebasPalmaSan <sebapalma59@gmail.com>
2025-05-12 17:01:06 -0300

    texto abous us

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -29,11 +29,10 @@ const AboutUs = () => {
             ¿Quiénes somos?
           </h2>
           <p className="flex justify-center font-secondary text-secondary-900 leading-relaxed text-base md:text-lg">
-            En Construir Valor, creamos más que obras: creamos confianza,
-            vínculos duraderos y espacios pensados. Desde nuestros comienzos,
-            asumimos un compromiso firme con la calidad, la innovación y la
-            responsabilidad, ofreciendo soluciones integrales en arquitectura,
-            ingeniería y construcción.
+            En Construir Valor, creamos más que obras: creamos confianza, vínculos duraderos y espacios pensados.
+            Desde nuestros comienzos, asumimos un compromiso firme con la calidad, la innovación y la responsabilidad, ofreciendo soluciones integrales en arquitectura, ingeniería y construcción. Con más de una década de experiencia en proyectos edilicios, comerciales, industriales y del ámbito público, nuestro equipo de profesionales combina pasión y conocimiento en cada obra.
+            Nuestro mayor diferencial es el acompañamiento cercano: guiamos a cada cliente en todas las etapas del proceso, desde la idea inicial hasta la entrega final, garantizando un servicio personalizado y una ejecución de excelencia. Los proyectos que realizamos se destacan por su funcionalidad, diseño y solidez, reflejando nuestra visión de construir con valor y para el futuro.
+
           </p>
         </div>
       </div>
